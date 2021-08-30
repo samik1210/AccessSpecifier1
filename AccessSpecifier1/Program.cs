7b005ceb9e153fd365db837481a66c4d0e70266d
parag <parag@DESKTOP-I2Q9QKQ>
2021-08-30 17:04:32 +0530

    Add project files.

---
+++ AccessSpecifier1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AccessSpecifier1
+{
+
+
+    class Student
+    {
+        public string name = "Samiksha";
+
+        public void print()
+        {
+            Console.WriteLine(" Student class");
+        }
+    }
+    class Student2
+    {
+        protected string name = "Samu";
+    }
+
+
+    class Program : Student2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Student student1 = new Student();
+            Console.WriteLine("Name: " + student1.name);
+            student1.print();
+            Console.ReadLine();
+
+            Program program = new Program();
+            Console.WriteLine("Name: " + program.name);
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Assembly1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Assembly1
+{
+    public class StudentName
+    {
+        internal string name = "Samiksha";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
